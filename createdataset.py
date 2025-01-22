569ecc345b8cb0d1dbadb7bc8706d63703b03f7c
francescouni0 <f.urso3@studenti.unipi.it>
2025-01-22 17:28:15 +0100

    dataset created

--- createdataset.py
+++ createdataset.py
@@ -18,48 +18,51 @@ cwd = os.getcwd()
 folder = Path()
 
 # Display the content of a folder
-output_folder = Path('./output')
+output_folder = Path('/media/francesco/HP P500/francesco/traindataset')
 
+a=[]
 
-for i in range(100, 103):
+for i in range(50, 250):
     f_d_edep = output_folder / f"dose3d_{i}.0_dose.mhd"
     d_edep = sitk.GetArrayFromImage(sitk.ReadImage(str(f_d_edep))).reshape(-1)
 
     n = len(d_edep)
     x = np.linspace(0, n, n)
     y = d_edep
-    plt.scatter(x, y, color='green', label='Dose')
-    plt.show()
 
-    histdose, bin_edges = np.histogram(x, weights=y, bins=50)
+    histdose, _ = np.histogram(x, weights=y, bins=50)
     histdose = (histdose / np.max(histdose)) * 100
-    bin_edges = np.linspace(-150, 150, len(bin_edges))
-    plt.plot(bin_edges[1:], histdose, color='green', label='Dose')
-    plt.legend()
-    plt.show()
 
-    f = uproot.open(f"output/phase_space{i}.0.root")
+    f = uproot.open(f"/media/francesco/HP P500/francesco/traindataset/phase_space{i}.0.root")
     ph = f['PhaseSpace']
-    ppx = ph['PostPosition_X'].array()
-    ppy = ph['PostPosition_Y'].array()
-    ppz = ph['PostPosition_Z'].array()
-    pre_dir_x = np.array(ph.arrays()['PreDirection_X'])
-    pre_dir_y = np.array(ph.arrays()['PreDirection_Y'])
-    pre_dir_z = np.array(ph.arrays()['PreDirection_Z'])
+    pre_dir = ph.arrays(['PreDirection_X','PreDirection_Y','PreDirection_Z'])
+    pre_dir_x = np.array(pre_dir['PreDirection_X'])
+    pre_dir_y = np.array(pre_dir['PreDirection_Y'])
+    pre_dir_z = np.array(pre_dir['PreDirection_Z'])
 
     angle_with_z = np.arccos(pre_dir_z / np.linalg.norm([pre_dir_x, pre_dir_y, pre_dir_z], axis=0)) * 180 / np.pi
-    ppz = np.array(ppz)
-    ppy = np.array(ppy)
-    ppx = np.array(ppx)
     angle_mask = np.logical_and(angle_with_z > 88, angle_with_z < 92)
-    filtered_ppz_angle = ppz[angle_mask]
-    filtered_ppy_angle = ppy[angle_mask]
-    filtered_ppx_angle = ppx[angle_mask]
+    filtered_ppz_angle = np.array(ph['PostPosition_Z'].array())[angle_mask]
+    filtered_ppy_angle = np.array(ph['PostPosition_Y'].array())[angle_mask]
 
-    h, xedges, yedges = np.histogram2d(filtered_ppz_angle, filtered_ppy_angle, bins=(50, 1), range=[[-149, 149], [-58, 58]])
-    h = np.reshape(h, (50))
+    h, xedges, yedges = np.histogram2d(filtered_ppz_angle, filtered_ppy_angle, bins=(50, 1),
+                                       range=[[-149, 149], [-58, 58]])
+    h = h.reshape((50))
     histgamma = (h / np.max(h)) * 100
-    bin_edges_g = np.linspace(-150, 150, len(xedges))
-    plt.plot(bin_edges_g[1:], histgamma, color='blue', label='gamma')
-    plt.legend()
-    plt.show()
+    ar_i=np.array([i])
+    ar_i = np.full(50, i)
+    row_data = np.vstack((histdose,ar_i, histgamma)).T
+    if i == 50:
+        a = row_data
+    else:
+        a = np.vstack((a, row_data))
+        
+
+print(np.shape(a))
+with open("dataset_matr.txt", "w") as f:
+    np.savetxt(f, a, fmt="%f")
+#z=np.linspace(-150, 150, 50)
+#
+#plt.plot(z, a[:50, 0], label='dose',color='green')
+#plt.plot(z,a[:50, 2], label='gamma',color='blue')
+#plt.show()
